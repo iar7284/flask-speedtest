46f7f2778f061655e8ce3e16e15ce1bc030de745
Imaniar Rusydiawan <imaniar.rusydiawan@Saptaindra.co.id>
2025-09-09 09:53:09 +0700

    adding script to check WIFI name

--- app/routes.py
+++ app/routes.py
@@ -16,6 +16,8 @@
 from flask import Blueprint, jsonify, render_template
 import speedtest
 import subprocess
+import platform
+import socket   
 import re
 
 # Create blueprint
@@ -23,15 +25,42 @@ main = Blueprint('main', __name__)
 
 def get_wifi_name():
     try:
+        # For Windows
+        if platform.system() == "Windows":
+            output = subprocess.check_output(['netsh', 'wlan', 'show', 'interfaces']).decode('utf-8')
+            if "There is no wireless interface on the system." in output:
+                return "No WiFi adapter available"
+            
+            network_info = {}
+            for line in output.split('\n'):
+                if "SSID" in line and "BSSID" not in line:
+                    network_info['ssid'] = line.split(":")[1].strip()
+                if "Signal" in line:
+                    network_info['signal'] = line.split(":")[1].strip()
+            
+            return network_info.get('ssid', 'Not connected')
+            
+        # For Linux (Azure App Service runs on Linux)
+        else:
+            try:
+                hostname = socket.gethostname()
+                return f"Server: {hostname}"
+            except:
+                return "Unable to get network information"
+                
+    except Exception as e:
+        return f"Network detection error: {str(e)}"
+    
+    #try:
         # Get WiFi info using netsh command
-        output = subprocess.check_output(['netsh', 'wlan', 'show', 'interfaces']).decode('utf-8')
+    #    output = subprocess.check_output(['netsh', 'wlan', 'show', 'interfaces']).decode('utf-8')
         # Find SSID in the output
-        ssid = re.search(r"SSID\s+: (.*)\r", output)
-        if ssid:
-            return ssid.group(1)
-        return "Not connected to WiFi"
-    except:
-        return "Unable to get WiFi information"
+    #    ssid = re.search(r"SSID\s+: (.*)\r", output)
+    #    if ssid:
+    #        return ssid.group(1)
+    #    return "Not connected to WiFi"
+    #except:
+    #    return "Unable to get WiFi information"
 
 @main.route('/')
 def index():
